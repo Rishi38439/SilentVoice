c110e789e211f010a13aee7bf770538b33ef6cf2
Rishi38439 <143995544+Rishi38439@users.noreply.github.com>
2025-02-01 09:40:32 +0530

    1st update by me(Rishi Singh Shekhawat)

--- silent_voice/lib/main.dart
+++ silent_voice/lib/main.dart
@@ -5,25 +5,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({super.key}); // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Container(
           color: const Color(0xFF97D8C4),
           child: Center(
-            child: Image.asset('assets/images/app_logo.png',
-                height:600,
-                width :600,
-                fit: BoxFit.cover,), 
-            
+            child: Image.asset(
+              'assets/images/app_logo.png',
+              height: 600,
+              width: 600,
+              fit: BoxFit.cover,
+            ),
           ),
-        )
-      )
-    );
+        )));
   }
 }
