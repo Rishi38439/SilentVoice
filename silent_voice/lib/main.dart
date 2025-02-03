f547ebf218f3901d7ddc36ac1f08479912dc8f47
Rishi38439 <143995544+Rishi38439@users.noreply.github.com>
2025-02-03 15:11:40 +0530

    commit2

--- silent_voice/lib/main.dart
+++ silent_voice/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login.dart'; // Importing the login.dart file
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +11,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: Container(
-          color: const Color(0xFF97D8C4),
-          child: Center(
-            child: Image.asset(
-              'assets/images/app_logo.png',
-              height: 600,
-              width: 600,
-              fit: BoxFit.cover,
-            ),
-          ),
-        )));
+        home: FutureBuilder(
+          future: Future.delayed(const Duration(seconds: 3), () {
+            return const LoginPage(); // Redirect to LoginPage after 3 seconds
+          }),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return const LoginPage(); // Show LoginPage
+            }
+            return Scaffold(
+              // Show loading screen while waiting
+              body: Container(
+                color: const Color(0xFF97D8C4),
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/app_logo.png',
+                    height: 600,
+                    width: 600,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
