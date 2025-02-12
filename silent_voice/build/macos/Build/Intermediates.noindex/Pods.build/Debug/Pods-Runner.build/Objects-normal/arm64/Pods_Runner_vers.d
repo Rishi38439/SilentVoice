0826dfffb1ae6cc75dd832eb137e91df0c4d5988
honey1229 <166580725+honey1229@users.noreply.github.com>
2025-02-12 08:32:59 +0530

    resolved merge conflicts

---
+++ silent_voice/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/honeymayurchauhan/Documents/GitHub/SilentVoice/silent_voice/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/honeymayurchauhan/Documents/GitHub/SilentVoice/silent_voice/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
---
+++ silent_voice/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation-dummy.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/honeymayurchauhan/Documents/GitHub/SilentVoice/silent_voice/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/honeymayurchauhan/Documents/GitHub/SilentVoice/silent_voice/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-dummy.m \
+  /Users/honeymayurchauhan/Documents/GitHub/SilentVoice/silent_voice/macos/Pods/Target\ Support\ Files/path_provider_foundation/path_provider_foundation-prefix.pch \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Cocoa.framework/Modules/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
