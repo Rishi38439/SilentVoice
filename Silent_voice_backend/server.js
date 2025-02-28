d91756d0580f1e7684191d6320692781bd263e10
Rishi38439 <143995544+Rishi38439@users.noreply.github.com>
2025-02-28 22:54:32 +0530

    i forgot to commit updated server.js file

--- Silent_voice_backend/server.js
+++ Silent_voice_backend/server.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
 const JWT_SECRET = process.env.JWT_SECRET || "your_secret_key";
@@ -47,4 +47,4 @@ app.post("/api/login", async (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0",() => console.log(`Server running on port ${PORT}`));
